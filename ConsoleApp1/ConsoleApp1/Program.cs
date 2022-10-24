8af60e5c36939e018e895840d51644c73c488553
Furkan Polat <95643569+FurkanADS01@users.noreply.github.com>
2022-10-24 18:23:58 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HaftaUc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uygulama 3
+            //Console.Write("Lütfen birinci sayıyı giriniz : ");
+            //int birinci = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Lütfen birinci sayıyı giriniz : ");
+            //int ikinci = Convert.ToInt32(Console.ReadLine());
+
+            //if (birinci % ikinci == 0)
+            //{
+            //    Console.WriteLine("Tam bölünüyor");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tam bölünemiyor");  
+            //}
+
+
+            // Uygulama 4
+            //Console.Write("gün yazınız :");
+            //int z = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("ay yazınız :");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("yıl yazınız :");
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //DateTime zaman = new DateTime(x, y, z);
+
+            //switch (y)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Ocak");
+            //        break;
+
+            //    case 2:
+            //        Console.WriteLine("Şubat");
+            //        break;
+
+            //    case 3:
+            //        Console.WriteLine("Mart");
+            //        break;
+
+            //    case 4:
+            //        Console.WriteLine("Nisan");
+            //        break;
+
+            //    case 5:
+            //        Console.WriteLine("Mayıs");
+            //        break;
+
+            //    case 6:
+            //        Console.WriteLine("Haziran");
+            //        break;
+
+            //    case 7:
+            //        Console.WriteLine("Temmuz");
+            //        break;
+
+            //    case 8:
+            //        Console.WriteLine("Ağustos");
+            //        break;
+
+            //    case 9:
+            //        Console.WriteLine("Eylül");
+            //        break;
+
+            //    case 10:
+            //        Console.WriteLine("Ekim");
+            //        break;
+
+            //    case 11:
+            //        Console.WriteLine("Kasım");
+            //        break;
+
+            //    case 12:
+            //        Console.WriteLine("Aralık");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Yanlış girdiniz");
+            //        break;
+            //}
+
+            // Uygulama 5
+            //Console.WriteLine("a,b,c değerlerini giriniz:");
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int c = int.Parse(Console.ReadLine());
+
+            //double delta = b * b - (4 * a * c);
+
+            //double x1, x2, x;
+
+            //if (delta > 0)
+            //{
+            //    x1 = (-b - (Math.Sqrt(delta))) / (2 * a);
+            //    x2 = (-b + (Math.Sqrt(delta))) / (2 * a);
+            //    Console.WriteLine("denklemin kökleri = "+x1+" ve "+x2);
+            //}
+            //else if (delta == 0)
+            //{
+            //    x = -b / (2 * a);
+            //    Console.WriteLine("denklemin kökü = "+x);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Kök yoktur");
+            //}
+
+
+            // Uygulama 6
+            //double derece, radyan, gradyan;
+            //double pi = 3.14;
+
+            //Console.Write("Açıyı giriniz : ");
+            //derece = Convert.ToDouble(Console.ReadLine());
+
+            //radyan = Convert.ToDouble(derece * pi / 180);
+            //gradyan = Convert.ToDouble(derece * 200 / 180);
+
+            //Console.WriteLine("Radyan : " + radyan);
+            //Console.WriteLine("Grad : " + gradyan);
+            //Console.ReadKey();
+        }
+    }
+}
